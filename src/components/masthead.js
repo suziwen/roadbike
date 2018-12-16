f6a689c02d21b4bbc3627723dae662a089991dda
suziwen <suziwen1@gmail.com>
2018-12-16 16:17:46 +0800

    修改图标

--- src/components/masthead.js
+++ src/components/masthead.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ArrowForwardIcon from "react-icons/lib/md/arrow-forward"
+import HomeIcon from "react-icons/lib/fa/home"
 
 import { rhythm, scale } from "../utils/typography"
 import presets, { colors } from "../utils/presets"
@@ -101,7 +102,7 @@ const MastheadContent = () => (
         </span>
         apps and websites with React
       </h1>
-      <Button large tag="href" target="_self" overrideCSS={{fontFamily: 'smy'}} to="http://markdown.xiaoshujiang.com" icon={<ArrowForwardIcon />}>
+      <Button large tag="href" target="_self" overrideCSS={{fontFamily: 'smy'}} to="http://markdown.xiaoshujiang.com" icon={<HomeIcon />}>
         开始写作
       </Button>
     </div>
