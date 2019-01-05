7132c3f0393a376dbd6d61f0bbc82a253fd3e733
suziwen <suziwen1@gmail.com>
2019-01-06 00:10:24 +0800

    gif 播放时提供加载中的动画提示

--- plugins/gatsby-transformer-xsjzip/src/gatsby-browser.js
+++ plugins/gatsby-transformer-xsjzip/src/gatsby-browser.js
@@ -34,6 +34,10 @@ exports.onRouteUpdate = () => {
 exports.onInitialClientRender = () => {
   $(document).on('click', '.story_block_image .gif_player', (e)=> {
     const target = e.currentTarget
+    target.classList.remove('error')
+    if (target.classList.contains('loading')){
+      return
+    }
     const img = target.querySelector('img')
     if (img.classList.contains('loaded')){
       if (target.classList.contains('playing')) {
@@ -41,9 +45,24 @@ exports.onInitialClientRender = () => {
         const still = img.dataset.still
         img.setAttribute('src', still)
       } else {
-        target.classList.add('playing')
         const src = img.dataset.src
-        img.setAttribute('src', src)
+        target.classList.add('loading');
+        const _img = document.createElement('img')
+        _img.onload = ()=>{
+          _img.onload = null
+          _img.onerror = null
+          target.classList.remove('loading')
+          target.classList.remove('error')
+          target.classList.add('playing')
+          img.setAttribute('src', src)
+        }
+        _img.onerror = ()=> {
+          _img.onload = null
+          _img.onerror = null
+          target.classList.remove('loading')
+          target.classList.add('error')
+        }
+        _img.setAttribute('src', src)
       }
     }
   })
