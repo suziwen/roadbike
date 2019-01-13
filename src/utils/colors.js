e11721554480a93cbc64eac6b8f2c268ded4ba87
suziwen <suziwen1@gmail.com>
2019-01-13 10:21:49 +0800

    代码块颜色删除

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -35,23 +35,6 @@ const colors = {
     light: gray(80, 270),
     superLight: gray(96, 270),
   },
-  code: {
-    bg: `#fdfaf6`, // colors.a[0] #fcf6f0
-    border: `#faede5`,
-    text: `#866c5b`,
-    remove: `#e45c5c`,
-    add: `#4a9c59`,
-    selector: `#b3568b`,
-    tag: `#4084a1`,
-    keyword: `#538eb6`,
-    comment: `#6f8f39`,
-    punctuation: `#53450e`,
-    regex: `#d88489`,
-    cssString: `#a2466c`,
-    invisibles: `#e0d7d1`,
-    scrollbarThumb: `#f4d1c6`,
-    lineHighlightBorder: `#f1beb6`,
-  },
 }
 
 export default colors
