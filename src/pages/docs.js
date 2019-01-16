d0d65acc7ae35e07e7e6ce767596f9c8bd108544
suziwen <suziwen1@gmail.com>
2019-01-16 14:28:23 +0800

    doc 主页说明

--- src/pages/docs.js
+++ src/pages/docs.js
@@ -15,98 +15,19 @@ class IndexRoute extends React.Component {
         {({data, set})=>{
           return (
               <DocSearchContent>
-                <Container>
+                <Container className="preview">
                   <Helmet>
-                    <title>Docs</title>
+                    <title>文档</title>
                   </Helmet>
                   <h1 id="get-started" css={{ marginTop: 0 }}>
-                    Get started
+                    小书匠官方文档手册
                   </h1>
-                  <p>Gatsby is a blazing fast modern site generator for React.</p>
+                  <p>该文档主要包含了小书匠编辑器支持的所有语法说明及小书匠使用教程。</p>
                   <p>
-                    This quick start is intended for intermediate to advanced
-                    developers. For a gentler intro to Gatsby,
-                    {` `}
-                    <Link to="/tutorial/">head to our tutorial!</Link>
+                    对 markdown 不是很了解的用户，建议详细阅读语法部份里的
+                    <Link to="/docs/grammar/common/italic/">标准语法</Link>。
                   </p>
-                  <h2>
-                    Install Gatsby
-                    {`'`}s command line tool
-                  </h2>
-                  <p>
-                    <code>npm install --global gatsby-cli</code>
-                  </p>
-                  <h2 id="using-the-gatsby-cli">Using the Gatsby CLI</h2>
-                  <ol>
-                    <li>
-                      <p>Create a new site.</p>
-                      <div className="gatsby-highlight" data-language="bash">
-                        <pre className="language-bash">
-                          <code className="language-bash">
-                            gatsby new gatsby-site
-                          </code>
-                        </pre>
-                      </div>
-                    </li>
-                    <li>
-                      <code>cd gatsby-site</code>
-                    </li>
-                    <li>
-                      <code>gatsby develop</code> — Gatsby will start a hot-reloading
-                      development environment accessible at
-                      {` `}
-                      <code>localhost:8000</code>
-                    </li>
-                    <li>
-                      Try editing the JavaScript pages in <code>src/pages</code>.
-                      Saved changes will live reload in the browser.
-                    </li>
-                    <li>
-                      <code>gatsby build</code> — Gatsby will perform an optimized
-                      production build for your site generating static HTML and
-                      per-route JavaScript code bundles.
-                    </li>
-                    <li>
-                      <code>gatsby serve</code> — Gatsby starts a local HTML server
-                      for testing your built site.
-                    </li>
-                  </ol>
-                  <p>
-                    To see detailed documentation for the CLI commands, run in the
-                    terminal <code>gatsby --help</code> and for specific commands
-                    {` `}
-                    <code>gatsby COMMAND_NAME --help</code> e.g.
-                    {` `}
-                    <code>gatsby develop --help</code>.
-                  </p>
-                  <h2 id="using-other-starters">Using other starters</h2>
-                  <p>
-                    Running <code>gatsby new</code> installs the default Gatsby
-                    starter. There are
-                    {` `}
-                    <Link to="/starters/">
-                      many other official and community starters
-                    </Link>
-                    {` `}
-                    you can use to kickstart building your Gatsby site.
-                  </p>
-                  <h2 id="work-through-the-tutorial">Work through the tutorial</h2>
-                  <p>
-                    It walks you through building a Gatsby site from scratch to a
-                    finished polished site.
-                    {` `}
-                    <Link to="/tutorial/">Go to the tutorial</Link>.
-                  </p>
-                  <div
-                    css={{
-                      display: `block`,
-                      [presets.Tablet]: {
-                        display: `none`,
-                      },
-                    }}
-                  >
-                    <h2>Documentation</h2>
-                  </div>
+                  
                 </Container>
               </DocSearchContent>
           )
