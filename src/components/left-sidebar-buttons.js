0044a4201a53f4f8643994ef403ea1fab30a5ce2
suziwen <suziwen1@gmail.com>
2019-01-16 12:05:32 +0800

    动画效果

--- src/components/left-sidebar-buttons.js
+++ src/components/left-sidebar-buttons.js
@@ -9,12 +9,33 @@ const ContainerStyled = styled('div')`
   position: fixed;
   right: 20px;
   display: none;
+  text-align: right;
   flex-direction: column;
   ${presets.Tablet}{
     display: flex;
   }
   ${presets.Desktop}{
     display: flex;
+  }
+  & .fas{
+    vertical-align: middle;
+  }
+  & .icon_tip{
+    font-size: .7em;
+    opacity: 0;
+    transition: transform 0.5s cubic-bezier(0.18, 0.89, 0.32, 1.28), opacity 0.5s ease-out, color 0.1s linear;
+    transform: translateX(-1em);
+    visibility: hidden;
+    background-color: inherit;
+    white-space: nowrap;
+    border-bottom: 1px dashed;
+    display: inline-block;
+    margin-right: 10px;
+  }
+  &:hover .icon_tip{
+    opacity: 1;
+    transform: none;
+    visibility: visible;
   }
 `
 
@@ -32,8 +53,8 @@ class LeftSidebarButtons extends React.Component {
     }
     return (
     <ContainerStyled>
-      {zipURL&&<a href={zipURL}><i className="fas fa-file-archive"></i></a>}
-      {pdfURL&&<Link to={pdfURL}><i className="fas fa-file-pdf"></i></Link>}
+      {zipURL&&<a href={zipURL}><span className="icon_tip">修改当前文件</span><i className="fas fa-file-archive"></i></a>}
+      {pdfURL&&<Link to={pdfURL}><span className="icon_tip">查看pdf版本</span><i className="fas fa-file-pdf"></i></Link>}
     </ContainerStyled>
       )
   }
