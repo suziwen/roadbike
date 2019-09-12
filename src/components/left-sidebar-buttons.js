aa595aff34304c1e71e5b4ffda1bba8b1eeabfc1
suziwen <suziwen1@gmail.com>
2019-09-12 10:53:47 +0800

    加入提示

--- src/components/left-sidebar-buttons.js
+++ src/components/left-sidebar-buttons.js
@@ -5,6 +5,19 @@ import { Link } from "gatsby"
 import presets, { colors } from "../utils/presets"
 import { rhythm, options } from "../utils/typography"
 
+// Access to global `localStorage` property must be guarded as it
+// fails under iOS private session mode.
+var hasLocalStorage = true
+var testKey = `gatsbyjs.sidebar.testKey`
+var ls
+try {
+  ls = global.localStorage
+  ls.setItem(testKey, `test`)
+  ls.removeItem(testKey)
+} catch (e) {
+  hasLocalStorage = false
+}
+
 const ContainerStyled = styled('div')`
   position: fixed;
   right: 20px;
@@ -17,6 +30,48 @@ const ContainerStyled = styled('div')`
   }
   ${presets.Desktop}{
     display: flex;
+  }
+  & .xsj_notice{
+    position: absolute;
+    margin-right: 1.6em;
+    z-index: 1;
+    text-align: center;
+    padding: 3px;
+    width: 200px;
+    right: 0;
+    top: 50%;
+    transform: translate(0, -50%);
+    color: ${colors.gatsby};
+    border: 1px solid ${colors.ui.border};
+    background-color: ${colors.ui.light};
+    font-size: .8em;
+    .i_got_it{
+      font-size: .6em;
+      color: ${colors.ui.border};
+    }
+    .i_got_it:hover{
+      color: ${colors.gatsby};
+    }
+    i{
+      position: absolute;
+      top: 50%;
+      left: 100%;
+      margin-top: -12px;
+      width: 12px;
+      height: 24px;
+      overflow: hidden;
+    }
+    i:after{
+      content: '';
+      position: absolute;
+      width: 12px;
+      height: 12px;
+      left: 0;
+      top: 50%;
+      transform: translate(-50%,-50%) rotate(-45deg);
+      background-color: ${colors.ui.light};
+      border: 1px solid ${colors.ui.border};
+    }
   }
   & .fas{
     vertical-align: middle;
@@ -41,8 +96,43 @@ const ContainerStyled = styled('div')`
 `
 
 class LeftSidebarButtons extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.closeNotice = this.closeNotice.bind(this)
+  }
+
+  _readLocalStorage(key) {
+    if (hasLocalStorage) {
+      return localStorage.getItem(`gatsbyjs:notice:${key}`)
+    } else {
+      return false
+    }
+  }
+
+  _writeLocalStorage(state, key) {
+    if (hasLocalStorage) {
+      localStorage.setItem(`gatsbyjs:notice:${key}`, JSON.stringify(state))
+    }
+  }
+  componentDidMount() {
+    if (hasLocalStorage) {
+      const xsjHasNoticed = this._readLocalStorage('xsjHasNoticed')
+      if (!!xsjHasNoticed){
+        this.setState({
+          hasNoticed: true
+        })
+      }
+    }
+  }
+  closeNotice(){
+    this._writeLocalStorage(true, 'xsjHasNoticed')
+    this.setState({
+      hasNoticed: true
+    })
+  }
   render() {
     const props = this.props
+    const state = this.state || {}
     let zipURL = ''
     let pdfURL = ''
     // when  gatsby build , the window will undefined
@@ -58,7 +148,15 @@ class LeftSidebarButtons extends React.Component {
     if (zipURL || pdfURL){
       return (
       <ContainerStyled>
-        {zipURL&&<a href={zipURL}><span className="icon_tip">修改当前文件</span><i className="fas fa-file-archive"></i></a>}
+        {zipURL&&<div css={{position: "relative"}}>
+          {!state.hasNoticed&&<div className="xsj_notice">点击这里，可以直接修改当前文件哦
+            <div>・ω・</div>
+            <div><a href="javascript:void;" onClick={this.closeNotice} className="i_got_it">我知道了</a></div>
+            <i></i>
+          </div>}
+          <a href={zipURL} target="_blank"><span className="icon_tip">修改当前文件</span><i className="fas fa-file-archive"></i></a>
+        </div>
+        }
         {pdfURL&&<Link to={pdfURL}><span className="icon_tip">查看pdf版本</span><i className="fas fa-file-pdf"></i></Link>}
       </ContainerStyled>
         )
