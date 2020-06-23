1182857e38e86b9853c56d7b2efb8ed2c30bffb9
suziwen <suziwen1@gmail.com>
2020-06-23 12:59:59 +0800

    release 8.2.0

--- src/pages/download.js
+++ src/pages/download.js
@@ -53,13 +53,13 @@ const DownloadSections = ({
       title={`当前最新版本 v${version}`}
     >
       <DownloadButtonsStyled>
-        <Button large to="https://github.com/suziwen/markdownxiaoshujiang/releases/tag/v8.1.1" ondark={true} tag="href" target="_blank" icon={<MdArrowForward />} style={{margin: "10px"}}>
+        <Button large to="https://github.com/suziwen/markdownxiaoshujiang/releases/tag/v8.2.0" ondark={true} tag="href" target="_blank" icon={<MdArrowForward />} style={{margin: "10px"}}>
           Github 下载
         </Button>
-        <Button large to="https://gitee.com/suziwen/v8.1.1/releases/v8.1.1" ondark={true} tag="href" target="_blank" icon={<MdArrowForward />} style={{margin: "10px"}}>
+        <Button large to="https://gitee.com/suziwen/v8.2.0/releases/v8.2.0" ondark={true} tag="href" target="_blank" icon={<MdArrowForward />} style={{margin: "10px"}}>
           Gitee 下载
         </Button>
-        <Button large to="https://pan.baidu.com/s/19OlyH_OT628dDapWCquvVA" ondark tag="href" target="_blank" icon={<MdArrowForward />} overrideCSS={{
+        <Button large to="https://pan.baidu.com/s/1R2mLjHruMtEgLt3RrylLgw" ondark tag="href" target="_blank" icon={<MdArrowForward />} overrideCSS={{
           margin: "10px",
           position: "relative",
           "&:after": {
@@ -69,7 +69,7 @@ const DownloadSections = ({
             right: `0`,
             pointerEvents: `none`,
             fontSize: `1rem`,
-            content: `'提取密码: (pr4q)'`,
+            content: `'提取密码: (drsf)'`,
           }
         }}>
           百度网盘下载
@@ -181,7 +181,7 @@ class IndexRoute extends React.Component {
               [presets.Hd]: { padding: vP, paddingTop: 0, paddingBottom: 0 },
             }}
           >
-            <DownloadSections version="8.1.1" downloadUrl="http://www.baidu.com" />
+            <DownloadSections version="8.2.0" downloadUrl="http://www.baidu.com" />
             <OldDownloadSections />
             <OtherDownloadSections />
           </div>
