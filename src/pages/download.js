3c9ac7d20856548650157ef9601a55f0e00cdc09
suziwen <suziwen1@gmail.com>
2021-05-29 14:53:06 +0800

    release android 0.0.2

--- src/pages/download.js
+++ src/pages/download.js
@@ -105,12 +105,12 @@ const DownloadSections = ({
 const AndroidDownloadSections = ()=>{
   return (
     <HomepageSection
-      sectionName="0.0.1"
+      sectionName="0.0.2"
       sectionIcon={PriceIcon}
       inverseStyle={true}
-      title={`花田日记 (Android 测试版本) v0.0.1`}
+      title={`花田日记 (Android 测试版本) v0.0.2`}
       links={[{
-        to: "https://github.com/suziwen/markdownxiaoshujiang/releases/tag/Android_Beta_0.0.1",
+        to: "https://github.com/suziwen/markdownxiaoshujiang/releases/tag/Android_Beta_0.0.2",
         label: "Github 下载",
         inverseStyle: true,
         icon: MdArrowForward,
