165c459dbbde371e1f525ef34681c8f2e780e8a8
suziwen <suziwen1@gmail.com>
2021-06-15 09:18:38 +0800

    android 0.0.6

--- src/pages/download.js
+++ src/pages/download.js
@@ -105,12 +105,12 @@ const DownloadSections = ({
 const AndroidDownloadSections = ()=>{
   return (
     <HomepageSection
-      sectionName="0.0.5"
+      sectionName="0.0.6"
       sectionIcon={PriceIcon}
       inverseStyle={true}
-      title={`花田日记 (Android 测试版本) v0.0.5`}
+      title={`花田日记 (Android 测试版本) v0.0.6`}
       links={[{
-        to: "https://github.com/suziwen/markdownxiaoshujiang/releases/tag/Android_Beta_0.0.5",
+        to: "https://github.com/suziwen/markdownxiaoshujiang/releases/tag/Android_Beta_0.0.6",
         label: "Github 下载",
         inverseStyle: true,
         icon: MdArrowForward,
