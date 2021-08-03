cb956f9f09f3ea9a35ba396423e20e9a11922500
suziwen <suziwen1@gmail.com>
2021-08-03 21:54:43 +0800

    release 8.6.2

--- src/pages/download.js
+++ src/pages/download.js
@@ -53,16 +53,16 @@ const DownloadSections = ({
       title={`当前最新版本 v${version}`}
     >
       <DownloadButtonsStyled>
-        <Button large to="https://github.com/suziwen/markdownxiaoshujiang/releases/tag/v8.6.1" ondark={true} tag="href" target="_blank" icon={<MdArrowForward />} style={{margin: "10px"}}>
+        <Button large to="https://github.com/suziwen/markdownxiaoshujiang/releases/tag/v8.6.2" ondark={true} tag="href" target="_blank" icon={<MdArrowForward />} style={{margin: "10px"}}>
           Github 下载
         </Button>
-        <Button large to="https://gitee.com/suziwen/v8.6.1/releases/v8.6.1" ondark={true} tag="href" target="_blank" icon={<MdArrowForward />} style={{margin: "10px"}}>
+        <Button large to="https://gitee.com/suziwen/v8.6.2/releases/v8.6.2" ondark={true} tag="href" target="_blank" icon={<MdArrowForward />} style={{margin: "10px"}}>
           Gitee 下载
         </Button>
         <Button large to="https://www.aliyundrive.com/s/Ss9LcYedT8g" ondark={true} tag="href" target="_blank" icon={<MdArrowForward />} style={{margin: "10px"}}>
           阿里云盘
         </Button>
-        <Button large to="https://pan.baidu.com/s/1VId0OxEsyCINxzzqp1X83Q" ondark tag="href" target="_blank" icon={<MdArrowForward />} overrideCSS={{
+        <Button large to="https://pan.baidu.com/s/1rgZNbHmTRZusc04KAMIkrQ" ondark tag="href" target="_blank" icon={<MdArrowForward />} overrideCSS={{
           margin: "10px",
           position: "relative",
           "&:after": {
@@ -72,7 +72,7 @@ const DownloadSections = ({
             right: `0`,
             pointerEvents: `none`,
             fontSize: `1rem`,
-            content: `'提取密码: (rb24)'`,
+            content: `'提取密码: (s9i2)'`,
           }
         }}>
           百度网盘下载
@@ -212,7 +212,7 @@ class IndexRoute extends React.Component {
             }}
           >
             <AndroidDownloadSections />
-            <DownloadSections version="8.6.1" downloadUrl="http://www.baidu.com" />
+            <DownloadSections version="8.6.2" downloadUrl="http://www.baidu.com" />
             <OldDownloadSections />
             <OtherDownloadSections />
           </div>
