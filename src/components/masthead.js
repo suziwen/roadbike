4b59ccb77e7255a510367c8eae4c6e5a094aba8d
suziwen <suziwen1@gmail.com>
2022-08-30 09:08:25 +0800

    tilt

--- src/components/masthead.js
+++ src/components/masthead.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import { IconContext } from "react-icons"
 import {GiFeather} from "react-icons/gi"
 import { RiBilibiliFill, RiYoutubeFill } from "react-icons/ri"
+import Tilt from 'react-parallax-tilt'
 
 import styled, {keyframes} from "react-emotion"
 import { rhythm, scale } from "../utils/typography"
@@ -20,6 +21,99 @@ const sheenEffect = keyframes({
   }
 })
 
+
+
+const StartWriteBtn = (props) => {
+  return (
+<Button large onClick={props.rippleEffect} tag="button" target="_self" overrideCSS={{
+          fontFamily: 'smy',
+          position: `relative`,
+          textShadow: `3px 3px 7px hsla(0, 2%, 13%, 0.3)`,
+          backgroundColor: `rgba(255, 229, 181, 0.5)`,
+          background: `rgba(255, 229, 181, 0.5)`,
+          color: colors.gray.lightCopy,
+          borderRadius: `30% 70% 70% 30% / 30% 30% 70% 70%`,
+          boxShadow: `-1px 0px 0 0.2rem rgba(255, 229, 181, 0.39)`,
+          overflow: `hidden`,
+          "&:hover, &:focus": {
+            color: colors.gray.dark,
+            backgroundColor: `rgba(255, 229, 181, 0.5)`,
+            backgroundImage: 'none',
+            animation: 'none',
+          },
+          "&::before": {
+            content: `""`,
+            display: `block`,
+            position: `absolute`,
+            background: `rgba(255, 255, 255, 0.5)`,
+            width: `60px`,
+            height: `100%`,
+            top: 0,
+            filter: `blur(30px)`,
+            transform: `translateX(-100px) skewX(-15deg)`,
+          },
+          "&::after": {
+            content: `""`,
+            display: `block`,
+            position: `absolute`,
+            background: `rgba(255, 255, 255, 0.2)`,
+            width: `30px`,
+            height: `100%`,
+            top: 0,
+            filter: `blur(5px)`,
+            transform: `translateX(-100px) skewX(-15deg)`,
+          },
+          "&:hover::before,&:hover::after": {
+            animation: `${sheenEffect} 1s ease infinite`,
+          },
+          "&>.crater": {
+            position: `absolute`,
+            backgroundColor: colors.gray.dark,
+            borderRadius: `100%`,
+          }
+
+        }} to="http://markdown.xiaoshujiang.com" icon={<GiFeather />}>
+          开始写作
+          <span className="crater" css={{
+            top: `18px`,
+            left: `10px`,
+            width: `4px`,
+            height: `4px`,
+          }}></span>
+          <span className="crater" css={{
+            top: `28px`,
+            left: `22px`,
+            width: `6px`,
+            height: `6px`,
+          }}></span>
+          <span className="crater" css={{
+            top: `10px`,
+            left: `25px`,
+            width: `8px`,
+            height: `8px`,
+          }}></span>
+          <span className="crater" css={{
+            bottom: `10px`,
+            right: `25px`,
+            width: `8px`,
+            height: `8px`,
+          }}></span>
+          <span className="crater" css={{
+            bottom: `28px`,
+            right: `22px`,
+            width: `6px`,
+            height: `6px`,
+          }}></span>
+          <span className="crater" css={{
+            bottom: `18px`,
+            right: `10px`,
+            width: `4px`,
+            height: `4px`,
+          }}></span>
+        </Button>
+  )
+
+}
 
 const MastheadContent = (props) => {
 
@@ -144,92 +238,9 @@ const MastheadContent = (props) => {
             </Link>
           </li> */}
       </ul>
-        <Button large onClick={props.rippleEffect} tag="button" target="_self" overrideCSS={{
-          fontFamily: 'smy',
-          position: `relative`,
-          textShadow: `3px 3px 7px hsla(0, 2%, 13%, 0.3)`,
-          backgroundColor: `rgba(255, 229, 181, 0.5)`,
-          background: `rgba(255, 229, 181, 0.5)`,
-          color: colors.gray.lightCopy,
-          borderRadius: `30% 70% 70% 30% / 30% 30% 70% 70%`,
-          boxShadow: `-1px 0px 0 0.2rem rgba(255, 229, 181, 0.39)`,
-          overflow: `hidden`,
-          "&:hover, &:focus": {
-            color: colors.gatsby,
-            backgroundColor: `rgba(255, 229, 181, 0.5)`,
-            backgroundImage: 'none',
-            animation: 'none',
-          },
-          "&::before": {
-            content: `""`,
-            display: `block`,
-            position: `absolute`,
-            background: `rgba(255, 255, 255, 0.5)`,
-            width: `60px`,
-            height: `100%`,
-            top: 0,
-            filter: `blur(30px)`,
-            transform: `translateX(-100px) skewX(-15deg)`,
-          },
-          "&::after": {
-            content: `""`,
-            display: `block`,
-            position: `absolute`,
-            background: `rgba(255, 255, 255, 0.2)`,
-            width: `30px`,
-            height: `100%`,
-            top: 0,
-            filter: `blur(5px)`,
-            transform: `translateX(-100px) skewX(-15deg)`,
-          },
-          "&:hover::before,&:hover::after": {
-            animation: `${sheenEffect} 1s ease infinite`,
-          },
-          "&>.crater": {
-            position: `absolute`,
-            backgroundColor: colors.gray.dark,
-            borderRadius: `100%`,
-          }
-
-        }} to="http://markdown.xiaoshujiang.com" icon={<GiFeather />}>
-          开始写作
-          <span className="crater" css={{
-            top: `18px`,
-            left: `10px`,
-            width: `4px`,
-            height: `4px`,
-          }}></span>
-          <span className="crater" css={{
-            top: `28px`,
-            left: `22px`,
-            width: `6px`,
-            height: `6px`,
-          }}></span>
-          <span className="crater" css={{
-            top: `10px`,
-            left: `25px`,
-            width: `8px`,
-            height: `8px`,
-          }}></span>
-          <span className="crater" css={{
-            bottom: `10px`,
-            right: `25px`,
-            width: `8px`,
-            height: `8px`,
-          }}></span>
-          <span className="crater" css={{
-            bottom: `28px`,
-            right: `22px`,
-            width: `6px`,
-            height: `6px`,
-          }}></span>
-          <span className="crater" css={{
-            bottom: `18px`,
-            right: `10px`,
-            width: `4px`,
-            height: `4px`,
-          }}></span>
-        </Button>
+      <Tilt perspective={500}>
+        <StartWriteBtn rippleEffect={props.rippleEffect}/>
+      </Tilt>
     </div>
       </IconContext.Provider>
   </div>
