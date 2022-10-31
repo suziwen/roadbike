615518d20ade151976fc03948a5e3e60be56abc9
suziwen <suziwen1@gmail.com>
2022-10-31 19:15:11 +0800

    android release 1.2.2

--- src/pages/download.js
+++ src/pages/download.js
@@ -144,12 +144,12 @@ const DockerDownloadSections = ()=>{
 const AndroidDownloadSections = ()=>{
   return (
     <HomepageSection
-      sectionName="1.2.1"
+      sectionName="1.2.2"
       sectionIcon={PriceIcon}
       inverseStyle={true}
       title={`有影笔记(小书匠手机版)`}
       links={[{
-        to: "https://github.com/suziwen/markdownxiaoshujiang/releases/tag/Android_1.2.1",
+        to: "https://github.com/suziwen/markdownxiaoshujiang/releases/tag/Android_1.2.2",
         label: "Android 下载(Github)",
         inverseStyle: true,
         icon: MdArrowForward,
