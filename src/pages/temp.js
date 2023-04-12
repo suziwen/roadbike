5300e2557a9f82edba4ff6950d9837bca25a05b3
suziwen <suziwen1@gmail.com>
2023-04-12 13:32:31 +0800

    font letter

--- src/pages/temp.js
+++ src/pages/temp.js
@@ -12,16 +12,16 @@ class IndexRoute extends React.Component {
           <div>
             <span css={{
               fontFamily: `founderkaiti`,
-            }}>从，知识到智慧，先化繁于简，再厚积薄发</span>
+            }}>从，知识到智慧，先化繁于简，再厚积薄发,术，0与1,简约却不简单</span>
           </div>
           <div>
             <span css={{
-            }}>小书匠，一款助你将混沌变为有序，令你事半功倍的知识管理软件。</span>
+            }}>小书匠，一款助你将混沌变为有序，令你事半功倍的知识管理软件。小书匠，一款本地优先，去中心化，自定义云同步服务策略的专业数字软件。</span>
           </div>
           <div>
             <span css={{
               fontFamily: `xsjkt`,
-            }}>如此而已，开始使用</span>
+            }}>如此而已，开始使用,开始通电，数字之美</span>
           </div>
         </div>
     )
